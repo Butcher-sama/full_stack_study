29bf89ebefc4961a0b37553c926904d3c5defa3b
Butcher-sama <beynitbey@gmail.com>
2025-03-28 15:54:14 +0300

    20.03.2025 c# files has been added

---
+++ 20.03.2025/dizi_array_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dizi_array_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string[] isimler = { "ayşe", "ali", "deniz" };
+			for(int i = 0; i < isimler.Length; i++)
+				Console.WriteLine("İsim listesi: " + isimler[i]);
+			Console.ReadLine();
+		}
+    }
+}
---
+++ 20.03.2025/diziler(array)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diziler_array_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Aynı türdeki birden fazla değeri tek bir değişkende saklamaya yarayan veri yapısı
+			// Sabit uzunluğa sahiptir
+			// İndeks numaraları ile elemanlara erişilir
+
+			int[] sayi = { 10, 20, 30, 40, 50 };
+			Console.WriteLine(sayi[0]);
+			Console.WriteLine(sayi[3]);
+			Console.ReadLine();
+        }
+    }
+}
