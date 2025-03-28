fa57615db81b323c71cb57fe161ce57d86e81baa
Butcher-sama <beynitbey@gmail.com>
2025-03-28 15:55:33 +0300

    Added missing files

---
+++ 20.03.2025/uygulama1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// kira hesabı yapan bir program yazalım
+			// Kullanıcıdan sözleşme başlangıç yılı, bitiş yılı, kira, yıllık kira artış miktarı istenecek
+			// Her yılın kirasını yazdıralım
+
+			Console.WriteLine("Sözleşmenin başlangıç yılını giriniz :");
+			int basyil = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Sözleşmenin bitiş yılını giriniz :");
+			int bityil = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Sözleşmenin başlangıç kira bedelini giriniz :");
+			int kira = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Kira yıllık artış yüzdesini giriniz :");
+			int yuzde = Convert.ToInt32(Console.ReadLine());
+
+			float toplamkira = kira;
+
+			while(basyil <= bityil)
+			{
+				Console.WriteLine(basyil + ". yılın kirası = " + toplamkira);
+				basyil++;
+				toplamkira += (toplamkira * yuzde) / 100;
+			}
+			Console.ReadLine();
+		}
+    }
+}
---
+++ 20.03.2025/uygulama2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// 1- 100 arasında rastgele tutulan sayıların 5'E bölünenlerin toplamı 1000 i bulan program
+
+			Random r = new Random();
+			int sayac = 0;
+		go:
+			int toplam = 0;
+		
+			while(toplam < 1000)
+			{
+				int nbr = r.Next(1, 101);
+				if(nbr % 5 == 0)
+				{
+					Console.WriteLine("5. e bölünen sayı : " + nbr);
+					toplam += nbr;
+				}
+			}
+			if (toplam != 1000)
+			{
+				sayac++;
+				Console.WriteLine(sayac + ". deneme başarızı. Hadi bir daha\n");
+				goto go;
+			}
+			else
+			{
+				Console.WriteLine("Program bitti. Toplam = " + toplam);
+				Console.WriteLine((sayac + 1) + ". denemede 1000 i bulduk");
+			}
+			Console.ReadLine();
+        }
+    }
+}
