c354a86371ce5e03a0ca45760bee3f5cabdace48
Butcher-sama <beynitbey@gmail.com>
2025-03-28 15:51:25 +0300

    Missing files added

---
+++ 14.03.2025/try_catch_ornek1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_catch_ornek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		// Try Bloğu:	Hata oluşabilecek kodları içerir
+		// Catch Bloğu:	Oluşan hataları yakalar ve programın çökmesini engeller
+		// Finally:		Her durumda çalışır ve en son yazılması istenen mesaj buraya yazılabilir
+
+		go:
+			try
+			{
+				Console.WriteLine("1. sayıyı giriniz: ");
+				int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+				Console.WriteLine("İkinci sayıyı giriniz: ");
+				int sayi2 = Convert.ToInt32(Console.ReadLine());
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Hata tespit edildi. Doğru formatta sayı giriniz!\n");
+				goto go; // işaretlenen yerden yeniden başlamasını sağlıyor
+			}
+			Console.ReadLine();
+		}
+    }
+}
---
+++ 14.03.2025/try_catch_ornek2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_catch_ornek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		go:
+			try
+			{
+				Console.WriteLine("Bir sayı girişi yapınız =");
+				string giris = Console.ReadLine();
+				int sayi = Convert.ToInt32(giris);
+
+				Console.WriteLine("Girdiğiniz sayı = " + sayi);
+			}
+			catch (FormatException) // sayı yerine başka bir karakter girilirse bu kısım çalışır
+			{
+				Console.WriteLine("Hatalı giriş! Lütfen sadece sayı giriniz!\n");
+				goto go;
+			}
+			catch (OverflowException) // çok büyük veya çok küçük sayı girildiğinde çalışır
+			{
+				Console.WriteLine("Hatalı giriş! Çok büyük veya çok küçük bir sayı girdiniz!\n");
+				goto go;
+			}
+			catch (Exception ex) // beklenmeyen diğer tüm hatalar için kullanılır
+			{
+				Console.WriteLine("Beklenmeyen bir hata oluştu: " + ex.Message);
+				goto go;
+			}
+			finally // her işlem sonucunda çalışacak
+			{
+				Console.WriteLine("İşlem tamamlandı");
+			}
+			Console.ReadLine();
+        }
+    }
+}
